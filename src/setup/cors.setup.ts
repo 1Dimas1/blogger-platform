27337595ac8bb038338ac90f1070b3307e56e16f
Dmytro Tsakhlo <dimassjavpg@gmail.com>
2025-11-18 14:05:09 +0200

    added proper cors set up

--- src/setup/cors.setup.ts
+++ src/setup/cors.setup.ts
@@ -1,5 +1,10 @@
 import { INestApplication } from '@nestjs/common';
 
 export function corsSetup(app: INestApplication) {
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 }
